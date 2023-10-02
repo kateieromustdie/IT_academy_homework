329d7ac178de61a77e3cbcc32347ec32aa64ee76
Kate Grigoryeva <ekaterin.grigoryeva@gmail.com>
2023-10-02 22:35:35 +0300

    “commit”

--- HomeWork4/file.js
+++ HomeWork4/file.js
@@ -1,1 +1,13 @@
+const fs = require('fs-extra');
 
+fs.ensureDir('folder1');
+fs.ensureFileSync('folder4/file1.txt');
+fs.ensureDir('folder2');
+fs.moveSync('folder1/file1.txt', 'folder2/file1.txt');
+fs.ensureDir('folder3');
+fs.copySync('folder2/file1.txt', 'folder3/file1.txt');
+fs.removeSync('folder2/file1.txt');
+fs.removeSync('folder3/file1.txt');
+fs.removeSync('folder1');
+fs.removeSync('folder2');
+fs.removeSync('folder3');
