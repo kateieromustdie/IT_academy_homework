5ff39f967e119bef2cde86c807f93a04a9fabfd3
Kate Grigoryeva <52533480+kateieromustdie@users.noreply.github.com>
2023-10-09 15:28:09 +0300

    Create HomeWork5.js

---
+++ HomeWork5.js
@@ -0,0 +1,21 @@
+let str = "name";
+let bool = true;
+let num = 43;
+
+console.log(str + bool);
+console.log(str + num);
+console.log(num + bool);
+
+console.log(str * bool);
+console.log(str * num);
+console.log(num * bool);
+
+console.log(str / bool);
+console.log(str / num);
+console.log(num / bool);
+
+let stringNum = "11";
+
+console.log(num.toString()); // "43"
+console.log(parseInt(stringNum)); // 11
+console.log(bool.toString()); //true
