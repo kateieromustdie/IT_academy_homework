a1d9dfb7ca61d5cf416dc8f549767023ea86c114
Kate Grigoryeva <52533480+kateieromustdie@users.noreply.github.com>
2023-10-18 00:20:06 +0300

    Update HomeWork7.4.js

--- HomeWork7/HomeWork7.4.js
+++ HomeWork7/HomeWork7.4.js
@@ -1,15 +1,20 @@
-const compareNumbers = (num1, num2) => { // что-то не работает тут всё равно
+const compareNumbers = (num1, num2) => {
     let stringNum1 = num1.toString();
     let stringNum2 = num2.toString()
     let samePositionValue = 0;
-    for(i = 0; i < 4; i++){
+    for(let i = 0; i < 4; i++){
         if(stringNum1[i] === stringNum2[i]) {
             samePositionValue++
         }
+    return samePositionValue;
     };
     let sameValue = 0;
-    for(i = 0; i < 4; i++){
-        if(stringNum2.includes(stringNum2[i])) {
+    for(let i = 0; i < 4; i++){
+        if(stringNum1.includes(stringNum2[i])) {
             sameValue++
     }
+    return sameValue;
+    
 }
+
+compareNumbers(4588, 2508);
