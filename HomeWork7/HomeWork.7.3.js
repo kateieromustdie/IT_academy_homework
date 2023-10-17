8f8c0dea6d58acff2dae2e58eed7f5f536e07c40
Kate Grigoryeva <52533480+kateieromustdie@users.noreply.github.com>
2023-10-17 11:20:11 +0300

    Update HomeWork.7.3.js

--- HomeWork7/HomeWork.7.3.js
+++ HomeWork7/HomeWork.7.3.js
@@ -1,1 +1,9 @@
+const fibonachi = (n, m) => {
+    let fibRow = [0, 1]; 
+    for (let i = 2; i < n + m; i++) { 
+      fibRow[i] = fibRow[i - 1] + fibRow[i - 2]; 
+    }
+    return fibRow.slice(n, n + m); 
+  }
 
+console.log(fibonachi(5, 8)); 
